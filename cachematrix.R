93b5cbf322f966025fec1abc4606d559fa42d682
Rolands Strickis <rolands.strickis@gmail.com>
2017-10-29 03:04:44 +0200

    added body to functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Here is the pair of functions that 
+## helps to compute inversion of square matrix and
+## helps to cache a result so it could be used further without repeated computing
+## Example:
+## mat <- makeCacheMatrix( matrix(c(1,2,3,4),2,2) )
+## inv <- cacheSolve(mat)
 
-## Write a short comment describing this function
+
+# Return a list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  
+  get <- function() x
+  
+  setmatrix <- function(inversion) m <<- inversion
+  
+  getmatrix <- function() m
+  
+  list(get = get, setmatrix = setmatrix, getmatrix = getmatrix)
+  
 }
 
-
-## Write a short comment describing this function
+# Return inverse matrix from cache or compute it if not cached
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$getmatrix()
+  
+  if(!is.null(m)) {
+    
+    message("Getting cached data")
+    return(m)
+    
+  }
+  
+  data <- x$get()
+  
+  if(det(data) == 0) {
+  
+    message("Matrix is not invertible")
+     
+  } else {
+  
+    m <- solve(data, ...)
+    
+    x$setmatrix(m)
+    
+    m
+    
+  }
+  
 }
